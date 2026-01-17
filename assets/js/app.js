6106d9a73f3e2b40f9e928597f260aa802daeeea
AlvarezF7 <alvarez.vic.382@gmail.com>
2026-01-16 23:22:53 -0300

    terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,69 @@
-//actividad cosumo API res rick y martin
+const url = 'https://rickandmortyapi.com/api/character/'
 const cards = document.querySelector("#showCard"); 
+const btnFiltro = document.querySelector("#btnF");
+const btnOrdenar = document.querySelector("#btnGrupo");
 
-fetch ("https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10")
-.then(response =>response.json())
-.then(data => {
-    const  especies = "Human";
-    const personajes = filtrarEspecie(data,especies);
-    mostrarCards(personajes);
-    console.log(data);
-});    
+let personajes= [];
+let primeros10=[];
 
-function mostrarCards(data){
-      
-        cards.innerHTML ="";
-        data.forEach(personaje =>{
-            cards.innerHTML +=`
-                <div class="cardBody">
-                    <img src="${personaje.image}">
-                    <ul>
-                        <li class="info">Nombre:${personaje.name}</li>
-                        <li class="info">Status:${personaje.status}</li>
-                        <li class="info">Especie:${personaje.species}</li>
-                    </ul>
-               
-                    
-                </div>
-        `;
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+   
+    personajes= data.results;
+    mostrarCards(personajes,data);
+  })
+  .catch(error => console.error('Error:', error));
 
+function mostrarCards(data) {
+  cards.innerHTML = "";
+  data.forEach(personaje => {
+    cards.innerHTML += `
+      <div class="cardBody">
+        <img src="${personaje.image}">
+        <ul>
+          <li class="info">Nombre: ${personaje.name}</li>
+          <li class="info">Status: ${personaje.status}</li>
+          <li class="info">Especie: ${personaje.species}</li>
+        </ul>
+      </div>
+    `;
+  });
+}
+
+btnFiltro.addEventListener("click", () => {
+ primeros10 = personajes.slice(0,10);
+  mostrarCards(primeros10);
+  console.log(primeros10);
 });
-}
 
-//funcion filtrar 
-function filtrarEspecie(data, especies){
-    return data.filter(personaje => personaje.species === especies);
-}
+btnOrdenar.addEventListener("click", () => {
+  const separados = primeros10.reduce((acc, p) => {
+    if (!acc[p.species]) acc[p.species] = [];
+    acc[p.species].push(p);
+    return acc;
+  }, {});
+
+  console.log(separados);
+
+  cards.innerHTML = "";
+
+for (let especie in separados) {
+    cards.innerHTML += `<p><strong>${especie}</strong></p>`;
+
+    separados[especie].forEach(personaje => {
+      cards.innerHTML += `
+        <div class="cardBody">
+          <img src="${personaje.image}">
+          <ul>
+            <li class="info">Id: ${personaje.id}</li>
+            <li class="info">Nombre: ${personaje.name}</li>
+            <li class="info">Status: ${personaje.status}</li>
+            <li class="info">Especie: ${personaje.species}</li>
+          </ul>
+        </div>
+      `;
+    });
+  }
+});
 
